3331cffbcee4d82ae2032222b1e32ed0af4981c6
MohamedLamineF <fadimoh@hotmail.fr>
2024-12-10 22:35:10 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,35 @@
-import "./App.css";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div
-      className={`min-h-screen bg-black transition-all duration-1000 ease-in-out flex flex-col items-center justify-between p-4 animate-gradient-x`}
-    >
-      <div className="grid grid-cols-1 md:grid-cols-3 items-center gap-2 w-full text-white/80 text-xs">
-        <p className="mr-2  text-center md:text-start truncate ">
-          Made for FreeCodeCamp course "Front End Development Libraries"
-        </p>
-        <div className="flex items-center justify-center gap-3">
-          <a
-            href="https://github.com/MohamedLamineF/Random-DZ-Quote-Machine"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-white transition-colors"
-          >
-            Github
-          </a>
-          <span className="text-white/50">•</span>
-          <a
-            href="https://www.linkedin.com/in/mohamedlaminef/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-white transition-colors"
-          >
-            LinkedIn
-          </a>
-          <span className="text-white/50">•</span>
-          <a
-            href="https://x.com/Mohamed_LamineF"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-white transition-colors"
-          >
-            Twitter
-          </a>
-          <span className="text-white/50">•</span>
-          <a
-            href="https://bsky.app/profile/mohamedlamine.bsky.social"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-white transition-colors"
-          >
-            Bluesky
-          </a>
-        </div>
-        <span className="mr-2 text-center md:text-end">© Lamine</span>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    </div>
-  );
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
